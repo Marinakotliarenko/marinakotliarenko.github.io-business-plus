6179a40581eab8fee5345b5c9f043b385811c2dd
Marina <marinakotliarenko@gmail.com>
2016-12-03 19:31:17 +0200

    contact-us-page

--- js-libraries/bootstrap-4.0.0-alpha.5/Gruntfile.js
+++ js-libraries/bootstrap-4.0.0-alpha.5/Gruntfile.js
@@ -143,7 +143,7 @@ module.exports = function (grunt) {
       options: {
         inject: 'js/tests/unit/phantom.js'
       },
-      files: 'js/tests/blog.html'
+      files: 'js/tests/blog-page.html'
     },
 
     // CSS build configuration
@@ -268,7 +268,7 @@ module.exports = function (grunt) {
           concurrency: 10,
           maxRetries: 3,
           maxPollRetries: 4,
-          urls: ['http://127.0.0.1:3000/js/tests/blog.html?hidepassed'],
+          urls: ['http://127.0.0.1:3000/js/tests/blog-page.html?hidepassed'],
           browsers: grunt.file.readYAML('grunt/sauce_browsers.yml')
         }
       }
