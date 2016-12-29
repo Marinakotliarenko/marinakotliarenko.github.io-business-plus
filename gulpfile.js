5271c07a0949893e1daee1590a85fae623bebd6a
Marina <marinakotliarenko@gmail.com>
2016-12-29 16:32:12 +0200

    - gulp haml compiler

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,27 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var haml = require('gulp-ruby-haml');
 
 gulp.task('sass', function(){
-    return gulp.src('app/scss/*.scss')
-        .pipe(sass({outputStyle: 'compressed'})) // Using gulp-sass
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', function(e) { console.log(e.message);})) // Using gulp-sass
         .pipe(gulp.dest('dist/css'))
 });
 
-gulp.task('watch', function(){
-    gulp.watch('app/scss/*.scss', ['sass']);
+gulp.task('haml', function() {
+    gulp.src('app/haml/**/*.haml', {read: false}).
+    pipe(haml().on('error', function(e) { console.log(e.message); })).
+    pipe(gulp.dest('dist/html'));
+});
+
+gulp.task('watch-scss', function(){
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('watch-haml', function() {
+    gulp.src('app/haml/**/*.haml', {read: false}).
+    pipe(watch('app/haml/**/*.haml')).
+    pipe(haml()).
+    pipe(gulp.dest('dist/html'));
 });
